3e04f76618c8b217f23d18dab41b0cb55993e106
kaili <373986529@qq.com>
2017-08-11 15:50:28 +0800

    update

---
+++ pages/send_log/send_log.js
@@ -0,0 +1,90 @@
+// send_log.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    img:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+  send_img:function(){
+    var that = this;
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths[0];
+        that.data.img.push(tempFilePaths);
+        that.setData({
+          img: that.data.img
+        })
+        wx.uploadFile({
+          url: 'http://119.29.140.135/setRunImg', //仅为示例，非真实的接口地址
+          filePath: tempFilePaths,
+          name: 'file',
+          success: function (res) {
+            var data = res.data
+            //do something
+          }
+        })
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
